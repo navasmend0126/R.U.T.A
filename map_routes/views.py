e7f2f5373e8e1b0f1f01e062f6ffcb0876ff2f99
navasmend0126 <omar.navas@utp.ac.pa>
2024-11-22 15:49:13 -0500

    Added .html .js and .css

--- map_routes/views.py
+++ map_routes/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse, request
 
 def index(request):
-    return HttpResponse("Hello world")
+    return render(request, 'map.html')
